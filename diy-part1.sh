87d3f0549883b25ee4e4b3d26a13f644ad0acb5e
lyb-human <85479650+lyb-human@users.noreply.github.com>
2023-01-18 12:43:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
+#sed -i "/helloworld/d" "feeds.conf.default"
 
 # Add a feed source
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 #echo 'src-git AdguardHome https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
